9aa6a04c3e43ef8abf022b57f3790e7dbe6ee5a9
BrianBeilby <brianbeilby98@gmail.com>
2023-10-20 16:21:50 -0700

    KFC-38: Added Username/Password Fields
    Login form now has username and password text entry box fields. Note: No styling or logic has been added. The form submit button routes user to /inventory without checking.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,49 @@
 import Nav from "../components/Nav"
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import {  useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    navigate('/inventory');
+  };
+
   return (
     <>
       <h1>Login</h1>
-        <button>
-          <Link to="/inventory">Login button</Link>
-        </button>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 }
