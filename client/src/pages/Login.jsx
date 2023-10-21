7819714a1fb64591d9526f421b8172ba8eaa2585
BrianBeilby <brianbeilby98@gmail.com>
2023-10-20 21:35:49 -0700

    KFC-38 Added styling and password visibility toggle to the login page using MUI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,51 @@
-import Nav from "../components/Nav"
-import { useState } from 'react';
-import {  useNavigate } from "react-router-dom";
+import Nav from "../components/Nav";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+const StyledTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "white",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "white",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "white",
+    },
+    "&:hover fieldset": {
+      borderColor: "white",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "white",
+    },
+  },
+  "& .MuiOutlinedInput-input": {
+    color: "white",
+  },
+  "& .MuiInputLabel-root": {
+    color: "white",
+  },
+  "& .MuiOutlinedInput-notchedOutline": {
+    borderColor: "white",
+  },
+});
 
 export default function Login() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -15,34 +55,69 @@ export default function Login() {
     setPassword(e.target.value);
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    navigate('/inventory');
+    navigate("/inventory");
   };
 
   return (
     <>
       <h1>Login</h1>
       <form onSubmit={handleFormSubmit}>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input
-            type="text"
+        <Box
+          sx={{ display: "flex", flexDirection: "column", gap: 1 }}
+        >
+          <StyledTextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            color="primary"
             id="username"
             value={username}
             onChange={handleUsernameChange}
+            InputProps={{
+              style: {
+                color: "white",
+              },
+            }}
           />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
+          <StyledTextField
+            label="Password"
+            variant="outlined"
+            fullWidth
             id="password"
+            type={showPassword ? "text" : "password"}
             value={password}
             onChange={handlePasswordChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    sx={{
+                      color: "white",
+                      "&:focus": {
+                        outline: "none",
+                      },
+                      "&:hover": {
+                        backgroundColor: "#323235",
+                      },
+                    }}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
-        </div>
-        <button type="submit">Login</button>
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </Box>
       </form>
     </>
   );
