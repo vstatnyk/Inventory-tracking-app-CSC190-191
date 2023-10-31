2d3f02fbc9515251edb3195746f0cf106112b497
Bohdan Hrytsak <locan.smiff@gmail.com>
2023-10-30 22:45:18 -0700

    Added a cancel button, fixed ID, fixed some vars

--- client/src/components/InventoryList.jsx
+++ client/src/components/InventoryList.jsx
@@ -1,98 +1,124 @@
 import React, { useState, useEffect } from "react";
 
-export default function Inventory({ items }) {
-    const [editState, setEditState] = useState({});
-    const [inputValues, setInputValues] = useState({});
-    const [newItem, setNewItem] = useState("");
-    const [inventoryItems, setInventoryItems] = useState([
-        { id: 1, name: "Item 1", stock: 10, checkedOut: 2 },
-        { id: 2, name: "Item 2", stock: 5, checkedOut: 1 },
-    ]);
+export default function inventoryList({ items }) {
+  const [editState, setEditState] = useState({});
+  const [inputValues, setInputValues] = useState({});
+  const [newItem, setNewItem] = useState("");
+  const [inventoryItems, setInventoryItems] = useState(items);
 
-    const nextItemId = Math.max(...inventoryItems.map((item) => item.id), 0) + 1;
+  const nextItemId = Math.max(...inventoryItems.map((item) => item.id), 0) + 1;
 
-    useEffect(() => {
-        const initialInputValues = {};
-        inventoryItems.forEach((item) => {
-            Object.keys(item).forEach((property) => {
-                initialInputValues[`${property}${item.id}`] = item[property];
-            });
-        });
-        setInputValues(initialInputValues);
-    }, [inventoryItems]);
+  useEffect(() => {
+    const initialInputValues = {};
+    inventoryItems.forEach((item) => {
+      Object.keys(item).forEach((property) => {
+        initialInputValues[`${property}${item.id}`] = item[property];
+      });
+    });
+    setInputValues(initialInputValues);
+  }, [inventoryItems]);
 
-    const handleEditClick = (itemId) => {
-        setEditState((prevState) => ({
-            ...prevState,
-            [itemId]: !prevState[itemId],
-        }));
+  const handleEditClick = (itemId) => {
+    setEditState((prevState) => ({
+      ...prevState,
+      [itemId]: !prevState[itemId],
+    }));
+  };
+
+  const handleInputChange = (property, value, item) => {
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [`${property}${item.id}`]: value,
+    }));
+  };
+
+  const handleSaveClick = (itemId) => {
+    const updatedItem = { ...inventoryItems.find((i) => i.id === itemId) };
+    Object.keys(updatedItem).forEach((property) => {
+      updatedItem[property] = inputValues[`${property}${itemId}`];
+    });
+
+    setInventoryItems((items) =>
+      items.map((item) => (item.id === itemId ? updatedItem : item))
+    );
+    handleEditClick(itemId); // Close the edit mode
+  };
+
+  const handleAddItem = () => {
+    const newItemObject = {
+      id: nextItemId,
+      name: "New Item",
+      stock: 0,
+      checkedOut: 0,
     };
+    setInventoryItems([...inventoryItems, newItemObject]);
+  };
 
-    const handleInputChange = (property, value, item) => {
-        const updatedItem = { ...item, [property]: value };
-        setInventoryItems((items) =>
-            items.map((i) => (i.id === item.id ? updatedItem : i))
-        );
-    };
+  const handleDeleteItem = (itemId) => {
+    const updatedItems = inventoryItems.filter((item) => item.id !== itemId);
+    setInventoryItems(updatedItems);
+  };
 
-    const handleAddItem = () => {
-        const newItemObject = {
-            id: nextItemId,
-            name: "New Item",
-            stock: 0,
-            checkedOut: 0,
-        };
-        setInventoryItems([...inventoryItems, newItemObject]);
-    };
-
-    const handleDeleteItem = (itemId) => {
-        const updatedItems = inventoryItems.filter((item) => item.id !== itemId);
-        setInventoryItems(updatedItems);
-    };
-
-    return (
-        <div className="inventoryContainer">
-          <button onClick={handleAddItem}>Add Item</button>
-          {inventoryItems.map((item) => (
-            <div className="inventoryItem" key={item.id}>
-              {Object.keys(item).map((property) => (
-                <div className="inventoryRow" key={property}>
-                  {property}:
-                  <input
-                    type="text"
-                    className="inventoryInput"
-                    style={{
-                      border: editState[item.id]
-                        ? "1px solid white"
-                        : "1px solid transparent",
-                    }}
-                    disabled={!editState[item.id]}
-                    id={`${property}${item.id}`}
-                    value={inputValues[`${property}${item.id}`]}
-                    onChange={(e) =>
-                      handleInputChange(property, e.target.value, item)
-                    }
-                  />
-                  <br />
-                </div>
-              ))}
+  return (
+    <div className="inventoryContainer">
+      <button onClick={handleAddItem}>Add Item</button>
+      {inventoryItems.map((item) => (
+        <div className="inventoryItem" key={item.id}>
+          {Object.keys(item).map((property) => (
+            <div className="inventoryRow" key={property}>
+              {property}:
+              <input
+                type="text"
+                className="inventoryInput"
+                style={{
+                  border: editState[item.id]
+                    ? "1px solid white"
+                    : "1px solid transparent",
+                }}
+                disabled={!editState[item.id]}
+                id={`${property}${item.id}`}
+                value={inputValues[`${property}${item.id}`]}
+                onChange={(e) =>
+                  handleInputChange(property, e.target.value, item)
+                }
+              />
               <br />
-              <div className="inventoryButtons"> {/* Wrap buttons in a div */}
+            </div>
+          ))}
+          <br />
+          <div className="inventoryButtons">
+            {editState[item.id] ? (
+              <>
+                <button
+                  id={`saveButton${item.id}`}
+                  onClick={() => handleSaveClick(item.id)}
+                >
+                  Save
+                </button>
                 <button
                   id={`button${item.id}`}
                   onClick={() => handleEditClick(item.id)}
                 >
-                  {editState[item.id] ? "Save" : "Edit"}
+                  Cancel
                 </button>
-                <button
-                  id={`button${item.id}`}
-                  onClick={() => handleDeleteItem(item.id)}
-                >
-                  Delete Item
-                </button>
-              </div>
-            </div>
-          ))}
+              </>
+            ) : (
+              <button
+                id={`button${item.id}`}
+                onClick={() => handleEditClick(item.id)}
+              >
+                Edit
+              </button>
+            )}
+            <button
+              id={`button${item.id}`}
+              onClick={() => handleDeleteItem(item.id)}
+            >
+              Delete Item
+            </button>
+          </div>
         </div>
-      );
+      ))}
+    </div>
+  );
 }
--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -1,5 +1,5 @@
 import Nav from "../components/Nav"
-import Inventorylist from "../components/InventoryList"
+import InventoryList from "../components/InventoryList"
 
 export default function Inventory() {
   // array for testing
@@ -7,14 +7,14 @@ export default function Inventory() {
     {id: 1, name: "item 1", stock: 3, checkedOut: 0},
     {id: 2, name: "item 2", stock: 4, checkedOut: 1},
     {id: 3, name: "item 3", stock: 2, checkedOut: 5},
-    {id: 4, name: "", stock: 1, checkedOut: 1},
+    {id: 4, name: "item 4", stock: 1, checkedOut: 1},
   ];
 
   return (
     <>
       <h1>Inventory</h1>
       <Nav/>
-      <Inventorylist items={items}/>
+      <InventoryList items={items}/>
     </>
   );
 }
