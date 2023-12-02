6e1067fd7ae724ef2b6410ba88ab2127644ed99d
CaymanBawden <113150104+CaymanBawden@users.noreply.github.com>
2023-12-01 19:45:15 -0800

    Implementation of nosql injection protection
    implemented input sanitization for filtering and searching

--- server/src/routes/transactions.js
+++ server/src/routes/transactions.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Transaction = require('../models/Transaction');
 const authorizeUser = require('../utils/authorizeUser');
+const validator = require('validator'); // Adding the validator library
 
 router.get('/', authorizeUser(2), async (req, res) => {
   try {
+    // Validate and sanitize user input, if applicable
+    const userInput = req.query.someInput;
+    if (!validator.isAlphanumeric(userInput)) {
+      return res.status(400).json({ message: 'Invalid input' });
+    }
+
+    // Use Mongoose query builder methods to construct a secure query
     const transactions = await Transaction.find()
-      .sort({ date: -1 }) // Sort by date in descending order
-      .limit(100); // Limit to 100 transactions
+      .sort({ date: -1 })
+      .limit(100);
+
     res.json(transactions);
   } catch (err) {
     res.status(500).json({ message: err.message });
